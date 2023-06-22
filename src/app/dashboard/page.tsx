01593f7c8a0ff276fbcb1e0a211918fe3f299c92
luchaox <lucasmiguel7@hotmail.com>
2023-06-22 18:31:35 -0300

    Implementando formulário do dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,18 +7,18 @@ export default function Dashboard() {
 
     const cookie = cookies();
 
-    const token = cookie.get('shoopypainel.token')
+    const token = cookie.get('bibliotechpainel.token')
 
-    if (!token?.value || verificaTokenExpirou(token.value)) {
+    /*if (!token?.value || verificaTokenExpirou(token.value)) {
         redirect('/login')
-    }
+    }*/
 
     return (
         <LayoutDashboard
             active='dashboard'
-            token={token.value}
+            /*token={token.value}*/
         >
-            <h1>Dashboard</h1>
+            <h1>Seja Bem-vindo...</h1>
         </LayoutDashboard>
     )
 }
