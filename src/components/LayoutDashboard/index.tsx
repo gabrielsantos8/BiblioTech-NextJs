f6ba25942651124c68e60d51da16f30acbe7c044
luchaox <lucasmiguel7@hotmail.com>
2023-06-23 18:05:39 -0300

    Finalizando o design do dashboard

--- src/components/LayoutDashboard/index.tsx
+++ src/components/LayoutDashboard/index.tsx
@@ -3,8 +3,10 @@
 import { validaPermissao } from "@/services/Token"
 import Link from "next/link"
 import { ReactNode } from "react"
-import { Dashboard, Header, Logo, NavItem, Page, SideNav } from "./style"
+import { Content, ContentText, ContentTitle, Dashboard, Divisor, ImgLogo, Sidebar, SidebarLink, SidebarTitle, Space, SpaceDiv } from "./style"
 import { Nav } from "react-bootstrap"
+import { PersonFill, BuildingFill, MortarboardFill, GearFill, Escape, PeopleFill, HouseDoorFill, BookFill, PenFill, PersonCircle, ChevronDown } from 'react-bootstrap-icons';
+
 
 interface interfProps {
     children: ReactNode
@@ -16,21 +18,84 @@ export const LayoutDashboard = (props: interfProps) => {
     return (
         <>
             <Dashboard>
-                <Header>
-                    <Logo>Dashboard</Logo>
-                </Header>
-                <SideNav>
-                    <NavItem href="#">Usuários</NavItem>
-                    <NavItem href="#">Alunos</NavItem>
-                    <NavItem href="#">Livros</NavItem>
-                    <NavItem href="#">Cursos</NavItem>
-                    <NavItem href="#">Editora</NavItem>
-                    <NavItem href="#">Autor</NavItem>
-                </SideNav>
-                <Page>
-                    {props.children}
-                </Page>
-            </Dashboard>
+      <Sidebar>
+        <SidebarTitle><ImgLogo src="img/dashboardlogo.png" /></SidebarTitle>
+        <Divisor/>
+        <SidebarLink href="#">
+            <HouseDoorFill className="sidebar-icon" />
+            Dashboard
+        </SidebarLink>
+        <SidebarLink href="#">
+            <PersonFill className="sidebar-icon" />
+            Usuários
+        </SidebarLink>
+        <SidebarLink href="#">
+            <PeopleFill className="sidebar-icon" />
+            Alunos
+        </SidebarLink>
+        <SidebarLink href="#">
+            <BookFill className="sidebar-icon" />
+            Livros
+        </SidebarLink>
+        <SidebarLink href="#">
+            <MortarboardFill className="sidebar-icon" />
+            Cursos
+        </SidebarLink>
+        <SidebarLink href="#">
+            <BuildingFill className="sidebar-icon" />
+            Editora
+        </SidebarLink>
+        <SidebarLink href="#">
+            <PenFill className="sidebar-icon" />
+            Autor
+        </SidebarLink>
+            
+        <SpaceDiv>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Space/>
+        </SpaceDiv>
+        <Divisor/>
+
+        <SidebarLink href="#">
+            <PersonCircle className="sidebar-icon" />
+            <ChevronDown className="sidebar-icon" />  
+        </SidebarLink>
+        <SidebarLink href="#">
+            <GearFill className="sidebar-icon" />
+            Configurações
+        </SidebarLink>       
+        <SidebarLink href="#">
+            <Escape className="sidebar-icon" />
+            Sair
+        </SidebarLink>
+
+
+        
+
+
+      </Sidebar>
+      <Content>
+        <ContentTitle>Dashboard</ContentTitle>
+        <ContentText>Seja bem-vindo...</ContentText>
+      </Content>
+    </Dashboard>
 
 
         </>
--- src/components/LayoutDashboard/style.ts
+++ src/components/LayoutDashboard/style.ts
@@ -2,43 +2,72 @@ import styled from 'styled-components';
 
 export const Dashboard = styled.div`
   display: flex;
+  height: 100vh;
+  background-color: #2b3035;
+  color: #adb5bd;
 `;
 
-export const Header = styled.header`
-  background-color: #222;
-  color: #fff;
-  padding: 10px;
-  position: fixed;
+export const Divisor = styled.hr``;
+export const SpaceDiv = styled.div``;
+export const Space = styled.br``;
+
+export const ImgLogo = styled.img`
   width: 100%;
 `;
 
-export const Logo = styled.h1`
-  color: #fff;
-  margin: 0;
-  font-size: 24px;
-`;
 
-export const SideNav = styled.nav`
-  width: 220px;
-  height: 100vh;
-  background-color: #333;
-  color: #fff;
+export const Sidebar = styled.div`
+  flex: 0 0 317px;
   padding: 20px;
+  background-color: #212529;
+  
+  
 `;
 
-export const NavItem = styled.a`
+export const SidebarTitle = styled.h6`
+  margin-top: 0;
+  margin-bottom: 1rem;
+  font-size: 14px;
+  text-transform: uppercase;
+  color: #6c757d;
+`;
+
+export const SidebarLink = styled.a`
   display: block;
-  color: #fff;
+  padding: 8px 1.5rem;
+  color: #00adef;
   text-decoration: none;
-  margin-bottom: 10px;
-  font-size: 16px;
+  font-size: 14px;
+  margin-left: -20px;
 
   &:hover {
-    text-decoration: underline;
+    color: #0d6efd;
+    background-color: rgba(13, 110, 253, 0.1);
+  }
+
+  .sidebar-icon {
+    margin-right: 0.5rem;
   }
 `;
 
-export const Page = styled.div`
+export const User = styled.div`
+  font-size: 30px;
+  
+`;
+
+
+export const Content = styled.div`
   flex: 1;
   padding: 20px;
+  
 `;
+
+export const ContentTitle = styled.h1`
+  margin-top: 0;
+  color: #adb5bd;
+`;
+
+export const ContentText = styled.p`
+  color: #adb5bd;
+`;
+
